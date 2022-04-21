418c398453e25a8a82a7dceb4224a1c0b4c76583
“amandajene08” <amandajene08@gmail.com>
2022-04-21 02:39:52 -0400

    TimingEventsExer

---
+++ app.js
@@ -0,0 +1,7 @@
+// Timing Events Exercise
+// 1. Use a setTimeout method to have "Hi" displayed as a p element/tag inside the div with the id of "first" 1 second after the page loads.
+// 2. Use nested setTimeout methods to complete the following: Have the text of "One" displayed as a p element/tag inside the div with the id of "timeout-nesting" 2 seconds after the page loads. Also, below the "One" p element/tag, have the text of "Two" also displayed as a p element/tag inside the same div 3 seconds after the page loads.
+// 3a.  Use a setInterval method to display a counter in the console that starts at 1 and continues going up one number at a time every second.
+// 3b. Have the "STOP" button set up so that when it is pressed the setInterval method created in 3a will stop continuously running (HINT: Need to create a variable).
+
+// BONUS
